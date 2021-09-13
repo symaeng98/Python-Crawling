3292b484bff491ca4adb0762811cfbaf363c38b7
symaeng98 <symaeng98@naver.com>
2021-09-14 01:37:08 +0900

    이코테

---
+++ 이코테/정렬/퀵 정렬(간결 ver).py
@@ -0,0 +1,20 @@
+##함수 선언 부분##
+def quick_sort(array):
+    #리스트가 하나 이하의 원소만을 담고 있으면 종료
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0] #첫 번째 원소가 피벗
+    tail = array[1:] #피벗을 제외한 리스트
+
+    left_side = [x for x in tail if x <= pivot] #분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] #분할된 오른쪽 부분
+
+    #분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬을 수행하고, 전체 리스트를 반환
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+##변수 선언 부분##
+
+##메인 함수 부분##
+if __name__ == "__main__":
+    array = [5,7,9,0,3,1,6,2,4,8]
+    print(quick_sort(array))
