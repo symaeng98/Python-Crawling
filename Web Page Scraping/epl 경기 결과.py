8c64178f04b3490bbf82143e86dbad58392bb5aa
symaeng98 <symaeng98@naver.com>
2021-09-13 18:20:58 +0900

    웹 스크래핑

--- Web Page Scraping/epl 경기 결과.py
+++ Web Page Scraping/epl 경기 결과.py
@@ -1,28 +1,35 @@
 from selenium import webdriver
-import csv
-import requests
 from bs4 import BeautifulSoup
 
-##selenium으로 일정 클릭
-browser = webdriver.Chrome()
-url = "https://www.spojoy.com/sportsinfo/infomation/league/?country=eng&category=soccer&game_gubun=D"
+browser = webdriver.Chrome("C:\PythonPractice\Python\Web Page Scraping\chromedriver.exe") #'./chromedriver'
+browser2 = webdriver.Chrome("C:\PythonPractice\Python\Web Page Scraping\chromedriver.exe") #'./chromedriver'
+# browser.maximize_window() #창 최대화
+url = 'https://www.flashscore.co.kr/soccer/england/premier-league/archive/'
 browser.get(url)
-elem = browser.find_element_by_id("img2")
-elem.click()
 
-##title 스크래핑
-filename = "EPL경기 결과.csv"
-f = open(filename, "w", encoding="utf-8-sig", newline="")
-writer = csv.writer(f)
-title = "시간	대회 홈팀	vs	원정팀 결과".split()
-writer.writerow(title)
+#지정한 위치로 스크롤 하기
+browser.execute_script("window.scrollTo(0,300)") #300크기만큼 스크롤 내림
 
-##내용 스크래핑
-res = requests.get(url)
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "lxml")
-data_rows = soup.find_all("table", attrs={"id":"stbl"}).find("tbody").find_all("tr")
-for row in data_rows:
-    columns = row.find_all("td")
-    data = [column.get_text() for column in columns]
-    print(data)
+#화면 가장 아래로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+#아카이브 페이지에서 시즌 태그 중에 첫번쨰 불러오기
+browser.find_element_by_link_text("프리미어리그 2021/2022").click()
+
+#시즌 페이지에서 결과창 불러오기
+browser.execute_script("window.scrollTo(0,300)") #300크기만큼 스크롤 내림
+browser.find_element_by_link_text("결과").click()
+
+
+#결과 창에서 id찾기
+browser.execute_script("window.scrollTo(0,300)") #300크기만큼 스크롤 내림
+soup = BeautifulSoup(browser.page_source,'lxml')
+id_arr = soup.find_all('div', attrs={'class':'event__match event__match--static event__match--twoLine'})
+
+for id in id_arr:
+    id['id'] = id['id'][4:]
+    print(id['id'])
+
+#새로운 창에 대해 url 얻기
+url_for_statistic = f"https://www.flashscore.co.kr/match/{id_arr[0]['id']}/#match-summary/match-summary"
+browser2.get(url_for_statistic)
+browser2.find_element_by_link_text("통계").click()
